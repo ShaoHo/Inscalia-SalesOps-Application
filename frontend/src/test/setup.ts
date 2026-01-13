3c27cba3c44efd7c07af915ab5a31718514bc271
Urocissa <hotingshao@gmail.com>
2026-01-13 21:52:36 +0800

    Add SalesOps UI screens and tests

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,1 +1,9 @@
-import "@testing-library/jest-dom";
+import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { afterEach, expect } from "vitest";
+
+afterEach(() => {
+  cleanup();
+});
+
+expect.extend(matchers);
